de1cd1619d3813c3271257f4169ff4ea2b9e5d7e
NalinDalal <nalindalal2004@gmail.com>
2024-07-01 19:49:15 +0530

    routing improved

--- Web/medium/frontend/src/App.tsx
+++ Web/medium/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import { Blog } from "./pages/Blog";
